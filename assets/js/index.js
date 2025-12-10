c55877cef0174a2e271e3b48de8af828f51e262a
POLIVAF <reikikapablo@gmail.com>
2025-12-09 22:36:46 -0300

    mi primer commit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+// Detectar el click en el botón
+document.getElementById("btn-generar").addEventListener("click", function () {
+  // Mostrar en el cuadrado
+
+  // Captura de datos
+  let nombre = prompt("Ingresa tu nombre:");
+  let apellido = prompt("Ingresa tu apellido:");
+
+  // Procesamiento del texto
+  let inicial = nombre.slice(0, 1).toLowerCase();
+  let tresLetrasApellido = apellido.slice(0, 3).toLowerCase();
+
+  // Generación de número aleatorio entre 10 y 99
+  let numeroAleatorio = Math.floor(Math.random() * 90) + 10;
+
+  // Ensamblaje final
+  let nombreDeUsuario = inicial + tresLetrasApellido + numeroAleatorio;
+
+  // Salida en consola
+  console.log("Tu nuevo nombre de usuario es: " + nombreDeUsuario);
+
+  // Mostrar en alerta
+    alert("Tu nuevo nombre de usuario es: " + nombreDeUsuario);
+});
